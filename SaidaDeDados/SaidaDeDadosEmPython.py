c68a75c7ed73b55bfc672fc5d6b1798ea407a74f
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-18 11:16:21 -0300

    Saida de dados em Python!

---
+++ SaidaDeDados/SaidaDeDadosEmPython.py
@@ -0,0 +1,11 @@
+idade: int
+salario: float
+nome: str
+sexo: str
+
+idade = 31
+salario = 4000.0
+nome = "Gabriel SD"
+sexo = 'M'
+
+print(f"O(A) funcionario(a) {nome}, sexo {sexo}, ganha {salario:.2f} e tem {idade} anos")
