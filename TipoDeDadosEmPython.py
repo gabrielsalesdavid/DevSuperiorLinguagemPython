9e2da0c5fe357b6d3ffb029cfbe4b9bb5be02f63
Gabriel Sales David <gabrielsalesdavid@hotmail.com>
2024-04-18 10:56:37 -0300

    Tipos de dados em Python!

---
+++ TipoDeDadosEmPython.py
@@ -0,0 +1,16 @@
+idade: int
+salario: float; altura: float
+genero: str
+nome: str
+
+idade = 3
+salario = 4000.0
+altura = 1.80
+genero = 'M'
+nome = "Gabriel Sales"
+
+print(f"Idade =  {idade}")
+print(f"Salario = {salario:.2f}")
+print(f"Altura = {altura:.2f}")
+print(f"Genero = {genero}")
+print(f"Nome = {nome}")
